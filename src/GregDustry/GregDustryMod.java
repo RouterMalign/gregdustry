8118f7946121c340c845095c8ee1fcad68be3ea0
ManuWar73 <111298180+ManuWar73@users.noreply.github.com>
2023-01-28 10:41:24 +0100

    Update and rename src/example/ExampleJavaMod.java to src/GregDustry/GregDustryMod.java

---
+++ src/GregDustry/GregDustryMod.java
@@ -0,0 +1,23 @@
+package example;
+
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
+
+public class ExampleJavaMod extends Mod{
+
+    public ExampleJavaMod(){
+    }
+
+    @Override
+    public void loadContent(){
+        Log.info("Loading content.");
+        Log.info("Mod content loaded.");
+    }
+
+}
